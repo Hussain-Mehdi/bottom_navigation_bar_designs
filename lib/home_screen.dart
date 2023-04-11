ab0b6765359a676efd9bbcb832d27621371e926f
Hussain-Mehdi <75681588+Hussain-Mehdi@users.noreply.github.com>
2023-04-11 22:36:11 +0500

    designed bottom navigation bar

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,8 +3,18 @@ import 'package:flutter/src/widgets/placeholder.dart';
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double width = 60;
+  double height = 40;
+  double left = 7;
+  double previous = 7;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +22,7 @@ class HomeScreen extends StatelessWidget {
       bottomNavigationBar: myBottomNavigationBar(context),
       body: const SafeArea(
           child: Center(
-        child: Text("Hello World"),
+        child: Text("Bottom Navigation Bar"),
       )),
     );
   }
@@ -22,21 +32,62 @@ class HomeScreen extends StatelessWidget {
       padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
       child: Container(
         width: MediaQuery.of(context).size.width,
-        height: 60,
+        height: 55,
         decoration: BoxDecoration(
           color: Colors.black12,
           borderRadius: BorderRadius.circular(15),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: const [
-            Icon(Icons.home_rounded),
-            Icon(Icons.bookmark_rounded),
-            Icon(Icons.search_rounded),
-            Icon(Icons.more_vert_rounded)
-          ],
-        ),
+        child: Stack(fit: StackFit.loose, children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 250),
+            curve: Curves.easeInOutExpo,
+            margin: EdgeInsets.only(left: left),
+            height: height,
+            width: 65,
+            decoration: BoxDecoration(
+                color: Colors.amber, borderRadius: BorderRadius.circular(15)),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      positionTheBox(8);
+                    },
+                    child: const Icon(Icons.home_rounded)),
+                GestureDetector(
+                    onTap: () {
+                      previous = 7;
+                      positionTheBox(87);
+                      print("Hello==================");
+                    },
+                    child: const Icon(Icons.bookmark_rounded)),
+                GestureDetector(
+                    onTap: () {
+                      previous = 85;
+                      positionTheBox(167);
+                    },
+                    child: const Icon(Icons.search_rounded)),
+                GestureDetector(
+                    onTap: () {
+                      previous = 167;
+                      positionTheBox(247);
+                    },
+                    child: const Icon(Icons.more_vert_rounded))
+              ],
+            ),
+          ),
+        ]),
       ),
     );
+  }
+
+  positionTheBox(double leftMove) {
+    setState(() {
+      previous = leftMove;
+      left == leftMove ? left = previous : left = leftMove;
+    });
   }
 }
